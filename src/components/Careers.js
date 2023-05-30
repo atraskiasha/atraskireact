91b0207da9f22b3abeeea157d60e579e2c8192f2
atraski asha <atraskiashasharma@gmail.com>
2023-05-30 11:38:12 +0530

    server

--- src/components/Careers.js
+++ src/components/Careers.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import anish from './images/Anish.png'
+import abhy from './images/Abhyuday.png'
+import priti from './images/Priti.png'
+import riju from './images/Riju.png'
+import priyanka from './images/Priyanka.png'
+import mrimoy from './images/mrimoy.png'
+import tridip from './images/Tridip.png'
+import mansi from './images/Mansi.png'
+import abhishek from './images/Abhishek.png'
+import asha from './images/asha.png'
 
 const Careers = () => {
 
@@ -25,51 +35,117 @@ const Careers = () => {
 
      
         <div class="col-xl-3 col-sm-6 mb-5">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
-                <h5 class="mb-0">Manuella Nevoresky</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src={anish} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <h5 class="mb-0">Anish Dhar</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
                 <ul class="social mb-0 list-inline mt-3">
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-square-facebook"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-square-twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-square-instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-linkedin"></i></a></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-facebook"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-twitter"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-instagram"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-linkedin"></i></li>
                 </ul>
             </div>
         </div>
 
         
         <div class="col-xl-3 col-sm-6 mb-5">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-2.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
-                <h5 class="mb-0">Samuel Hardy</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src={abhy} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <h5 class="mb-0">Abhyuday Majhi</h5><span class="small text-uppercase text-muted">Marketing Executive</span>
                 <ul class="social mb-0 list-inline mt-3">
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-square-facebook"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-square-twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-square-instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-linkedin"></i></a></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-facebook"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-twitter"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-instagram"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-linkedin"></i></li>
                 </ul>
             </div>
         </div>
 
        
         <div class="col-xl-3 col-sm-6 mb-5">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-1.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
-                <h5 class="mb-0">Tom Sunderland</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src={priti} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <h5 class="mb-0">Priti Jhawar</h5><span class="small text-uppercase text-muted">Business Development Executive</span>
                 <ul class="social mb-0 list-inline mt-3">
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-square-facebook"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-square-twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-square-instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-linkedin"></i></a></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-facebook"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-twitter"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-instagram"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-linkedin"></i></li>
                 </ul>
             </div>
         </div>
 
         <div class="col-xl-3 col-sm-6 mb-5">
-            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-team/teacher-7.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src={priyanka} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <h5 class="mb-0">P R Executive</h5><span class="small text-uppercase text-muted">Priyanka Basu </span>
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><i class="fa-brands fa-square-facebook"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-twitter"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-instagram"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-linkedin"></i></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src={riju} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <h5 class="mb-0">Riju RjD</h5><span class="small text-uppercase text-muted">Graphic Designer</span>
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><i class="fa-brands fa-square-facebook"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-twitter"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-instagram"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-linkedin"></i></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src={mrimoy} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <h5 class="mb-0">Mrinmoy Banik </h5><span class="small text-uppercase text-muted">Business Development Executive </span>
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><i class="fa-brands fa-square-facebook"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-twitter"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-instagram"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-linkedin"></i></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src={tridip} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <h5 class="mb-0">Tridip Chowdhury</h5><span class="small text-uppercase text-muted">Social Media & Marketing Exicutive</span>
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><i class="fa-brands fa-square-facebook"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-twitter"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-instagram"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-linkedin"></i></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src={abhishek} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
                 <h5 class="mb-0">John Tarly</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
                 <ul class="social mb-0 list-inline mt-3">
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-square-facebook"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-square-twitter"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-square-instagram"></i></a></li>
-                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa-brands fa-linkedin"></i></a></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-facebook"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-twitter"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-instagram"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-linkedin"></i></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src={mansi} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <h5 class="mb-0">John Tarly</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><i class="fa-brands fa-square-facebook"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-twitter"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-instagram"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-linkedin"></i></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xl-3 col-sm-6 mb-5">
+            <div class="bg-white rounded shadow-sm py-5 px-4"><img src={asha} alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm"/>
+                <h5 class="mb-0">John Tarly</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+                <ul class="social mb-0 list-inline mt-3">
+                    <li class="list-inline-item"><i class="fa-brands fa-square-facebook"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-twitter"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-square-instagram"></i></li>
+                    <li class="list-inline-item"><i class="fa-brands fa-linkedin"></i></li>
                 </ul>
             </div>
         </div>
